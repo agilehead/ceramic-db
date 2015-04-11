9b6443bcf50fdcdab5449ae52373b440c07c60a7
Jeswin <jeswinpk@agilehead.com>
2015-04-11 13:55:47 +0530

    Use ObjectID directly from the underlying driver

--- test/mongodb-test.js
+++ test/mongodb-test.js
@@ -1,9 +1,9 @@
-var mongoBackend = require("ceramic-backend-mongodb");
+var backend = require("ceramic-backend-mongodb");
 var tests = require("./ceramic-db-test");
 var TEST_DB_NAME = "ceramic-db-unittest-db-temp";
 
 var getDb = function*() {
-    return yield* mongoBackend.MongoClient.connect({ database: TEST_DB_NAME });
+    return yield* backend.MongoClient.connect({ database: TEST_DB_NAME });
 };
 
 tests(getDb, "MongoDb");
--- test/nedb-test.js
+++ test/nedb-test.js
@@ -1,4 +1,16 @@
 /*
+var backend = require("ceramic-backend-nedb");
+var tests = require("./ceramic-db-test");
+var TEST_DB_NAME = "ceramic-db-unittest-db-temp";
+
+var getDb = function*() {
+    return yield* backend.MongoClient.connect({ database: TEST_DB_NAME });
+};
+
+tests(getDb, "NeDB");
+*/
+
+/*
 var tests = require("./ceramic-db-test");
 var TEST_DB_NAME = "ceramic-db-unittest-db-temp";
 var NeDBBackend = require("ceramic-backend-nedb");
